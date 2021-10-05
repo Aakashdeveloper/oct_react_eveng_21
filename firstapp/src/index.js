c8cdcca18eccb97b61f7894be08dcd8e544852aa
aakashdeveloper <ahanda205@gmail.com>
2021-10-05 21:38:22 +0530

    reactwalkthrough

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './header'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return(
+        <div>
+            <Header/>
+            <h1>My React APP</h1>
+            <h2>Let`s Start</h2>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+// var React = require('react');
+/*
+function App(){
+    return(
+        <h1>React APP</h1>
+    )
+}
+*/
