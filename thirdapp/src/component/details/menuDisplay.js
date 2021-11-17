9d3143ea4ea240593d7bb610e7bde738e9b3decf
aakashdeveloper <ahanda205@gmail.com>
2021-11-17 21:56:22 +0530

    detailspage

---
+++ thirdapp/src/component/details/menuDisplay.js
@@ -0,0 +1,62 @@
+import React,{Component} from 'react';
+class MenuDisplay extends Component{
+    orderId = [];
+
+    addItem = (id) => {
+        this.orderId.push(id)
+        this.props.finalOrder(this.orderId)
+    }
+
+    removeOrder = (id) => {
+        this.orderId.splice(this.orderId.indexOf(id),1)
+        this.props.finalOrder(this.orderId)
+    }
+
+    renderCart = (orders) => { 
+        if(orders){
+            return orders.map((item) => {
+                return(
+                    <b>{item},&nbsp;&nbsp;</b>
+                )
+            })
+        }
+    }
+    renderMenu = ({menuData}) => {
+        if(menuData){
+            return menuData.map((item) =>{
+                return(
+                    <div key={item.menu_id}>
+                        <div className="col-md-7 items">
+                            <b>{item.menu_id}</b> &nbsp;
+                            <img src={item.menu_image} style={{height:80,width:80}}/>
+                            &nbsp;&nbsp; {item.menu_name} - Rs.{item.menu_price}
+                        </div>
+                        <div className="col-md-4">
+                            <button className="btn btn-primary" onClick={() => {this.addItem(item.menu_id)}}>
+                                <span className="glyphicon glyphicon-plus"></span>
+                            </button> &nbsp;
+                            <button className="btn btn-danger" onClick={() => {this.removeOrder(item.menu_id)}}>
+                                <span className="glyphicon glyphicon-minus"></span>
+                            </button>
+                        </div>
+                    </div>
+                )
+            })
+        }
+    }
+    render(){
+        return(
+            <div>
+                <div className="col-md-12 bg-success">
+                    <h1>Item Added</h1>
+                    Item Number {this.renderCart(this.orderId)} Added
+                </div>
+                <div className="col-md-12 bg-info">
+                    {this.renderMenu(this.props)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MenuDisplay;
--- thirdapp/src/component/details/restDetails.js
+++ thirdapp/src/component/details/restDetails.js
@@ -1,16 +1,30 @@
 import React,{Component} from 'react';
 import axios from 'axios';
 import './details.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import MenuDisplay from './menuDisplay';
 
-const url = "https://zomatoajulypi.herokuapp.com/details"
+const url = "https://zomatoajulypi.herokuapp.com/details";
+const menu = "https://zomatoajulypi.herokuapp.com/menu"
 
 class Details extends Component{
     constructor(){
         super()
 
         this.state={
-            details:''
+            details:'',
+            menuList:'',
+            userItem:''
         }
+    }
+
+    proceed = () => {
+        sessionStorage.setItem('menu', this.state.userItem);
+    }
+
+    addToCart = (data) => {
+        this.setState({userItem:data})
     }
 
     render(){
@@ -23,7 +37,32 @@ class Details extends Component{
                         <h2>{this.state.details.restaurant_name}</h2>
                     </div>
                     <div className="panel-body">
-                    <h2>{details.restaurant_name}</h2>
+                        <img src={details.restaurant_thumb} alt={details.restaurant_thumb} className="sliderImage"/>
+                        <hr/>
+                        <h2>{details.restaurant_name}</h2>
+                        <h2>{details.rating_text}</h2>
+                        <h2>{details.average_rating} Stars</h2>
+                        <Tabs>
+                            <TabList>
+                                <Tab>Details</Tab>
+                                <Tab>Contact</Tab>
+                                <Tab>Menu</Tab>
+                            </TabList>
+
+                            <TabPanel>
+                                <p>{details.restaurant_name} is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            </TabPanel>
+                            <TabPanel>
+                                <h2>Contact Number: {details.contact_number}</h2>
+                                <h2>Address: {details.address}</h2>
+                            </TabPanel>
+                            <TabPanel>
+                               <h2>Menu</h2>
+                               <MenuDisplay menuData={this.state.menuList} 
+                               finalOrder={(data) => {this.addToCart(data)}}/>
+                            </TabPanel>
+                        </Tabs>
+                        <button className="btn btn-success" onClick={this.proceed}>Proceed</button>
                     </div>
                 </div>
             </div>
@@ -34,8 +73,9 @@ class Details extends Component{
     //api call 
     async componentDidMount(){
         const restId = this.props.match.params.restid;
-        let response = await axios.get(`${url}/${restId}`)
-        this.setState({details:response.data[0]})
+        let response = await axios.get(`${url}/${restId}`);
+        let menudata = await axios.get(`${menu}/${restId}`);
+        this.setState({details:response.data[0], menuList:menudata.data})
     }
 }
 
